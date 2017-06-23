7d0acc74b4927d5700047d5ebe23e402cd581036
Ellis Pritchard <ellis.pritchard@ft.com>
2017-06-23 16:02:08 +0100

    add documentaton config, hex config and update deps

--- mix.exs
+++ mix.exs
@@ -7,6 +7,10 @@ defmodule FettlePlug.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     source_url: "https://github.com/Financial-Times/fettle_plug",
+     description: description(),
+     package: package(),
+     docs: docs(),
      deps: deps()]
   end
 
@@ -16,6 +20,24 @@ defmodule FettlePlug.Mixfile do
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
     [extra_applications: [:logger]]
+  end
+
+  def package do
+    [
+      maintainers: ["Ellis Pritchard"],
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/Financial-Times/fettle_plug"} ]
+  end
+
+  defp description do
+    """
+    Plug support for Fettle health-checks.
+    """
+  end
+
+  def docs do
+    [main: "readme",
+     extras: ["README.md"]]
   end
 
   # Dependencies can be Hex packages:
@@ -31,7 +53,11 @@ defmodule FettlePlug.Mixfile do
     [
       {:plug, "~> 1.3"},
       {:poison, "~> 3.1"},
-      {:fettle, github: "Financial-Times/fettle"}
+      {:fettle, github: "Financial-Times/fettle"},
+      {:credo, "~> 0.5", only: [:dev, :test]},
+      {:mix_test_watch, "~> 0.3", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false},
+      {:dialyxir, "~> 0.5.0", only: [:dev], runtime: false}
     ]
   end
 end
